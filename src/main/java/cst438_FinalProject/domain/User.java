1ddaa7042647de7b6f4bec2c4a51cd4fa33f7ef6
Roger Dodger <lpaddikt@gmail.com>
2020-08-01 14:15:48 -0700

    Added getters and setters for User

--- src/main/java/cst438_FinalProject/domain/User.java
+++ src/main/java/cst438_FinalProject/domain/User.java
@@ -1,5 +1,6 @@
 package cst438_FinalProject.domain;
 
+import java.util.Objects;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -27,5 +28,92 @@ public class User {
     this.carid = carid;
     this.flightid = flightid;
     this.hotelid = hotelid;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getFname() {
+    return fname;
+  }
+
+  public void setFname(String fname) {
+    this.fname = fname;
+  }
+
+  public String getLname() {
+    return lname;
+  }
+
+  public void setLname(String lname) {
+    this.lname = lname;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public long getCarid() {
+    return carid;
+  }
+
+  public void setCarid(long carid) {
+    this.carid = carid;
+  }
+
+  public long getFlightid() {
+    return flightid;
+  }
+
+  public void setFlightid(long flightid) {
+    this.flightid = flightid;
+  }
+
+  public long getHotelid() {
+    return hotelid;
+  }
+
+  public void setHotelid(long hotelid) {
+    this.hotelid = hotelid;
+  }
+
+  @Override
+  public String toString() {
+    return "User{" +
+        "id=" + id +
+        ", fname='" + fname + '\'' +
+        ", lname='" + lname + '\'' +
+        ", email='" + email + '\'' +
+        ", carid=" + carid +
+        ", flightid=" + flightid +
+        ", hotelid=" + hotelid +
+        '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    User user = (User) o;
+    return id == user.id &&
+        carid == user.carid &&
+        flightid == user.flightid &&
+        hotelid == user.hotelid &&
+        fname.equals(user.fname) &&
+        lname.equals(user.lname) &&
+        email.equals(user.email);
   }
 }
