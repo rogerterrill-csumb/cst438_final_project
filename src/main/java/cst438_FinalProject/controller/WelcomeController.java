4def39c8e2959db4be9e40dff31606389f4f0869
carlos <K>
2020-08-13 18:37:59 -0700

    Fixing conflict

--- src/main/java/cst438_FinalProject/controller/WelcomeController.java
+++ src/main/java/cst438_FinalProject/controller/WelcomeController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import cst438_FinalProject.domain.*;
 import cst438_FinalProject.service.*;
+import cst438_FinalProject.repository.*;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -153,9 +154,18 @@ public class WelcomeController {
 			return "welcome";
 	}
 
+	@GetMapping(value="/details/car")
+	public String getCarDetails(
+	Model model) {
+		String awesome = carService.getReservationID(user.getEmail());
+		System.out.println("Found records");
+		System.out.println(awesome);
+		model.addAttribute("carID","Found Reservation ID: " + awesome);
+		return "welcome";
+	}
 
-@PostMapping(value = "/flight/new")
-public String createFlightReservation(
+	@PostMapping(value = "/flight/new")
+	public String createFlightReservation(
     @RequestParam("flightID") int flightID,
     @RequestParam("seatClass") String seatClass,
     @RequestParam("numPassengers") int numPassengers,
